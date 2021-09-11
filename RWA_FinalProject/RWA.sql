d3957a3a3cd8b174943d9955e8269c43eb53d3e4
Vedran Petrač <vedran.petrac18@gmail.com>
2021-09-12 00:01:29 +0200

    Update RWA.sql

--- RWA_FinalProject/RWA.sql
+++ RWA_FinalProject/RWA.sql
@@ -1,22 +1,333 @@
+USE [AdventureWorksOBP]
+GO
+/****** Object:  StoredProcedure [dbo].[CreateProizvod]    Script Date: 12.9.2021. 0:00:07 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+create proc [dbo].[CreateProizvod]
+@naziv nvarchar(50),
+@brojProizvoda nvarchar(25),
+@boja nvarchar(15),
+@minKolNaSkladistu smallint,
+@cijenaBezPDV money,
+@podkategorijaID int
+as
+begin
+	insert into Proizvod(Naziv,BrojProizvoda,Boja,MinimalnaKolicinaNaSkladistu,CijenaBezPDV,PotkategorijaID)
+	values (@naziv,@brojProizvoda,@boja, @minKolNaSkladistu,@cijenaBezPDV,@podkategorijaID)
+end
+GO
+/****** Object:  StoredProcedure [dbo].[DeleteKategorija]    Script Date: 12.9.2021. 0:00:07 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+create proc [dbo].[DeleteKategorija]
+@id int
+as
+begin
+delete from Kategorija where IDKategorija = @id
+end
+GO
+/****** Object:  StoredProcedure [dbo].[DeletePodkategorija]    Script Date: 12.9.2021. 0:00:07 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+create proc [dbo].[DeletePodkategorija]
+@id int
+as
+begin
+delete from Potkategorija where IDPotkategorija = @id
+end
+GO
+/****** Object:  StoredProcedure [dbo].[DeleteProizvod]    Script Date: 12.9.2021. 0:00:07 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+create proc [dbo].[DeleteProizvod]
+@id int
+as
+begin
+delete from Proizvod where IDProizvod = @id
+end
+GO
+/****** Object:  StoredProcedure [dbo].[GetDrzave]    Script Date: 12.9.2021. 0:00:07 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+--Drzava
+Create proc [dbo].[GetDrzave]
+as
+select * from Drzava
+GO
+/****** Object:  StoredProcedure [dbo].[GetGradovi]    Script Date: 12.9.2021. 0:00:07 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+--Gradovi
+Create proc [dbo].[GetGradovi]
+as
+select * from Grad
+GO
+/****** Object:  StoredProcedure [dbo].[GetKartica]    Script Date: 12.9.2021. 0:00:07 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+--Kartica
+create proc [dbo].[GetKartica]
+@IDKartica int
+as
+begin
+select * from KreditnaKartica
+where IDKreditnaKartica = @IDKartica
+end
+GO
+/****** Object:  StoredProcedure [dbo].[GetKategorija]    Script Date: 12.9.2021. 0:00:07 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+create proc [dbo].[GetKategorija]
+@id int
+as
+begin
+select * from Kategorija where IDKategorija = @id
+end
+GO
+/****** Object:  StoredProcedure [dbo].[GetKategorije]    Script Date: 12.9.2021. 0:00:07 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+--Kategorija
+create proc [dbo].[GetKategorije]
+as
+begin
+select * from Kategorija
+end
+GO
+/****** Object:  StoredProcedure [dbo].[GetKomercijalist]    Script Date: 12.9.2021. 0:00:07 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+Create proc [dbo].[GetKomercijalist]
+@id int
+as
+select * from Komercijalist
+where IDKomercijalist = @id
+GO
+/****** Object:  StoredProcedure [dbo].[GetKomercijalisti]    Script Date: 12.9.2021. 0:00:07 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+--Komercijalist
+Create proc [dbo].[GetKomercijalisti]
+as
+select * from Komercijalist
+GO
+/****** Object:  StoredProcedure [dbo].[GetKupac]    Script Date: 12.9.2021. 0:00:07 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
 --Kupac
 
-create proc GetKupac
+create proc [dbo].[GetKupac]
 @id int
 as
 begin
 select * from Kupac
 where Kupac.IDKupac = @id
 end
-go
+GO
+/****** Object:  StoredProcedure [dbo].[GetKupci]    Script Date: 12.9.2021. 0:00:07 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
 
-create proc GetKupci
+create proc [dbo].[GetKupci]
 as
 begin
 select * from Kupac
 end
-go
+GO
+/****** Object:  StoredProcedure [dbo].[GetPodkategorija]    Script Date: 12.9.2021. 0:00:07 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
 
-create proc UpdateKupac
+create proc [dbo].[GetPodkategorija]
+@id int
+as
+begin
+select * from Potkategorija where IDPotkategorija = @id
+end
+GO
+/****** Object:  StoredProcedure [dbo].[GetPotkategorije]    Script Date: 12.9.2021. 0:00:07 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+--Podkategorija
+create proc [dbo].[GetPotkategorije]
+as
+begin
+select * from Potkategorija
+end
+GO
+/****** Object:  StoredProcedure [dbo].[GetProizvod]    Script Date: 12.9.2021. 0:00:07 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+create proc [dbo].[GetProizvod]
+@id int
+as
+begin
+select * from Proizvod where IDProizvod = @id
+end
+GO
+/****** Object:  StoredProcedure [dbo].[GetProizvodi]    Script Date: 12.9.2021. 0:00:07 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+--Proizvod
+CREATE proc [dbo].[GetProizvodi]
+as
+begin
+select 
+Proizvod.*,
+Potkategorija.Naziv as 'PotkategorijaNaziv'
+from Proizvod
+inner join Potkategorija on Potkategorija.IDPotkategorija = Proizvod.PotkategorijaID
+end
+GO
+/****** Object:  StoredProcedure [dbo].[GetRacun]    Script Date: 12.9.2021. 0:00:07 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+create proc [dbo].[GetRacun]
+@id int
+as
+begin
+select * from Racun
+where IDRacun = @id
+end
+GO
+/****** Object:  StoredProcedure [dbo].[GetRacuniForKupac]    Script Date: 12.9.2021. 0:00:07 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+--Racun
+
+create proc [dbo].[GetRacuniForKupac]
+@KupacID int	
+as
+begin
+select * from Racun where KupacID = @KupacID
+end
+GO
+/****** Object:  StoredProcedure [dbo].[GetStavkeForRacun]    Script Date: 12.9.2021. 0:00:07 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+--Stavka
+
+CREATE proc [dbo].[GetStavkeForRacun]
+@RacunID int
+as
+begin
+select 
+Stavka.*,
+Proizvod.*
+from Stavka
+inner join Proizvod on Stavka.ProizvodID = Proizvod.IDProizvod
+where RacunID = @RacunID
+end
+GO
+/****** Object:  StoredProcedure [dbo].[InsertKategorija]    Script Date: 12.9.2021. 0:00:07 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+create proc [dbo].[InsertKategorija]
+@Naziv nvarchar(50)
+as
+begin
+insert into Kategorija (Naziv)
+values (@Naziv)
+end
+GO
+/****** Object:  StoredProcedure [dbo].[InsertPodkategorija]    Script Date: 12.9.2021. 0:00:07 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+create proc [dbo].[InsertPodkategorija]
+@Naziv nvarchar(50)
+as
+begin
+insert into Potkategorija (Naziv)
+values (@Naziv)
+end
+GO
+/****** Object:  StoredProcedure [dbo].[UpdateKategorija]    Script Date: 12.9.2021. 0:00:07 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+create proc [dbo].[UpdateKategorija]
+@id int,
+@Naziv nvarchar(50)
+as
+begin
+update Kategorija
+set Naziv = @Naziv
+where IDKategorija = @id
+end
+GO
+/****** Object:  StoredProcedure [dbo].[UpdateKupac]    Script Date: 12.9.2021. 0:00:07 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+create proc [dbo].[UpdateKupac]
 @id int,
 @ime nvarchar(50),
 @prezime nvarchar(50),
@@ -34,178 +345,41 @@ SET
 	@GradID = @GradID 
 WHERE Kupac.IDKupac = @id;
 end
-go
+GO
+/****** Object:  StoredProcedure [dbo].[UpdatePodkategorija]    Script Date: 12.9.2021. 0:00:07 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
 
---Drzava
-Create proc GetDrzave
-as
-select * from Drzava
-go
-
---Gradovi
-Create proc GetGradovi
-as
-select * from Grad
-go
-
---Komercijalist
-Create proc GetKomercijalisti
-as
-select * from Komercijalist
-go
-
-Create proc GetKomercijalist
-@id int
-as
-select * from Komercijalist
-where IDKomercijalist = @id
-go
-
---Racun
-
-create proc GetRacuniForKupac
-@KupacID int	
-as
-begin
-select * from Racun where KupacID = @KupacID
-end
-go
-
---Stavka
-
-create proc GetStavkeForRacun
-@RacunID int
-as
-begin
-select * from Stavka
-where RacunID = @RacunID
-end
-go
-
---Kartica
-create proc GetKartica
-@IDKartica int
-as
-begin
-select * from KreditnaKartica
-where IDKreditnaKartica = @IDKartica
-end
-go
-
---Proizvod
-create proc GetProizvodi
-as
-begin
-select * from Proizvod
-end
-go
-
-create proc GetProizvod
-@id int
-as
-begin
-select * from Proizvod where IDProizvod = @id
-end
-go
-
-
-
-create proc DeleteProizvod
-@id int
-as
-begin
-delete from Proizvod where IDProizvod = @id
-end
-go
-
---Podkategorija
-create proc GetPodkategorije
-as
-begin
-select * from Podkategorija
-end
-go
-
-create proc GetPodkategorija
-@id int
-as
-begin
-select * from Podkategorija where IDPodkategorija = @id
-end
-go
-
-create proc InsertPodkategorija
-@Naziv nvarchar(50)
-as
-begin
-insert into Podkategorija (Naziv)
-values (@Naziv)
-end
-go
-
-create proc UpdatePodkategorija
+create proc [dbo].[UpdatePodkategorija]
 @id int,
 @Naziv nvarchar(50)
 as
 begin
-update Podkategorija
-set
-	IDPodkategorija = @id,
-	Naziv = @Naziv
+update Potkategorija
+set Naziv = @Naziv
+where IDPotkategorija = @id
 end
-go
+GO
+/****** Object:  StoredProcedure [dbo].[UpdateProizvod]    Script Date: 12.9.2021. 0:00:07 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
 
-
-create proc DeletePodkategorija
-@id int
+create proc [dbo].[UpdateProizvod]
+@idProizvod int,
+@naziv nvarchar(50),
+@brojProizvoda nvarchar(25),
+@boja nvarchar(15),
+@minKolNaSkladistu smallint,
+@cijenaBezPDV money,
+@podkategorijaID int
 as
 begin
-delete from Podkategorija where Podkategorija = @id
+	update Proizvod
+	set Naziv = @naziv, BrojProizvoda = @brojProizvoda, Boja = @boja, MinimalnaKolicinaNaSkladistu = @minKolNaSkladistu, CijenaBezPDV = @cijenaBezPDV, PotkategorijaID = @podkategorijaID
+	where IDProizvod = @idProizvod
 end
-go
-
---Kategorija
-create proc GetKategorija
-as
-begin
-select * from Kategorija
-end
-go
-
-create proc InsertKategorija
-@Naziv nvarchar(50)
-as
-begin
-insert into Kategorija (Naziv)
-values (@Naziv)
-end
-go
-
-create proc GetKategorija
-@id int
-as
-begin
-select * from Kategorija where IDKategorija = @id
-end
-go
-
-create proc UpdateKategorija
-@id int,
-@Naziv nvarchar(50)
-as
-begin
-update Kategorija
-set
-	IDKategorija = @id,
-	Naziv = @Naziv
-end
-go
-
-
-create proc DeleteKategorija
-@id int
-as
-begin
-delete from Kategorija where Kategorija = @id
-end
-go
+GO
